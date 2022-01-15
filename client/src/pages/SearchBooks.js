69bd185b1177452500b756df5c60485a98472a1e
Eduard Voicu <iameduardvoicu@gmail.com>
2022-01-15 17:57:22 -0600

    refactored handlesavebook function

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -67,7 +67,11 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({
+        variables: {
+          input: bookToSave
+        },
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
@@ -76,7 +80,7 @@ const SearchBooks = () => {
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
-      console.error(err);
+      console.error(error);
     }
   };
 
